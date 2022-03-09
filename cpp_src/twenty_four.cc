37a766c3da83d26fc621b9c16a3aa49f78a24158
h2agrawa <h2agrawa@mc-3003-59.student.cs.uwaterloo.ca>
2022-03-09 13:36:01 -0500

    day 24: added some basis for optimization

--- cpp_src/twenty_four.cc
+++ cpp_src/twenty_four.cc
@@ -20,9 +20,15 @@ class Arg {
     void set_char (char ch) { arg_type = CHAR; Arg::ch = ch; }
     void set_int (int val) { arg_type = INT; Arg::val = val; }
     void set_none () { arg_type = NONE; }
+    void print() {
+        if (arg_type == CHAR) cout << ch;
+        else if (arg_type == INT) cout << val;
+    }
+
+    ARG_TYPES get_arg_type () { return arg_type; }
+    char get_char() { return ch; }
+    int get_val () { return val; }
 };
-
-
 
 class Op {
     OP_TYPES op;
@@ -42,15 +48,23 @@ class Op {
         Op::arg1 = arg1;
         Op::arg2 = arg2;
     }
+
+    void print () {
+        cout << op << " ";
+        arg1.print();
+        cout << " ";
+        arg2.print();
+        cout << endl;
+    }
+
+    OP_TYPES get_op () { return op; }
+    Arg get_arg1 () { return arg1; }
+    Arg get_arg2 () { return arg2; }
+
 };
-
-
-
 
 class ALU {
     int w, x, y, z;
-
-
 
     public:
     ALU (): w(0), x(0), y(0), z(0) {}
@@ -61,7 +75,7 @@ class ALU {
         else if (var == 'z') { return z; }
         else { cerr << "unexpected case in get_var : " << var << endl; exit(1); }
     }
-    int set_var (char var, int val) {
+    void set_var (char var, int val) {
         if (var == 'w') { w = val; }
         else if (var == 'x') { x = val; }
         else if (var == 'y') { y = val; }
@@ -69,13 +83,7 @@ class ALU {
         else { cerr << "unexpected case in set_var : " << var << endl; exit(1); }
     }
 
-
 };
-
-
-
-
-
 
 
 class MONAD {
@@ -83,21 +91,68 @@ class MONAD {
 
     public:
     MONAD(vector<Op> ops): ops(ops) {}
+
+    void optimize() {
+        bool optimized = false;
+
+        do {
+            optimized = false;
+
+            for (int i = 0; i < ops.size(); ++i) {
+                Op op = ops[i];
+
+
+                // mul with 0 is 0
+                if (op.get_op() == MUL && op.get_arg2().get_arg_type() == INT && op.get_arg2().get_val() == 0) {
+                    Op updated_op;
+                    // op.set_op()
+                }
+                
+            }
+
+        } while (optimized);
+    }
 };
-
-
-
-
-
-
-
-
-
 
 vector<Op> read_ops (string filename) {
     ifstream f(filename);
     string line;
+    vector<Op> ops;
 
+    while (getline(f, line)) {
+        // cout << "line is " << line << endl;
+        Op op;
+        if (line != "") {
+            Arg arg1, arg2;
+            
+            op.set_op(line);
+            
+            getline(f, line);
+            assert(line.length() == 1);
+            arg1.set_char(line.at(0));
+
+            getline(f, line);
+            if (line == "") {
+                arg2.set_none();
+            }
+            else if (isdigit(line.at(0)) || (line.at(0) == '-')) {
+                int var = stoi(line);
+                arg2.set_int(var);
+            }
+            else {
+                // cout << "line is " << line << endl;
+                assert(line.length() == 1);
+                assert(isalpha(line.at(0)));
+                arg2.set_char(line.at(0));
+            }
+
+            op.set_args(arg1, arg2);
+            ops.push_back(op);
+        }
+    }
+    // cout << "ops size is " << ops.size() << endl;
+    assert(ops.size() == 252); // num ops in 24.txt
+    return ops;
 }
 
 
@@ -105,5 +160,10 @@ vector<Op> read_ops (string filename) {
 
 
 int main() {
+    vector<Op> ops = read_ops("/Users/h2agrawa/now/Advent_of_code_2021/txts/24_copy.txt");
+    // for (int i = 0; i < ops.size(); ++i) {
+    //     ops[i].print();
+    // }
+
 
 }
